e960157f1ab5bd79890ef8c930c75ff504de4df9
rapidleech <mohdsabahat.123@gmail.com>
2021-01-15 19:20:19 +0530

    added base templates

--- flaskapp.py
+++ flaskapp.py
@@ -1,9 +1,47 @@
 
-from flask import Flask,render_template
+from flask import Flask,render_template,request
+from helper.helper import passgen, shorten_url
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
 
-    return 'Hello world!'
+    return render_template('home.html')
+
+@app.route('/passgen',methods=['GET','POST'])
+def generate_password() :
+
+    errors = {}
+
+    if request.method == 'POST' :
+        length = request.form['length']     #Extract length of password from form!
+
+        """
+        add_sym = request.form['symbol']    #Get value of Symbol checkbox!
+        add_num = request.form['number']    #Get value of number checkbox!
+        """
+
+        password = passgen(length)      #pass add_sym & add_num argument!
+
+        return render_template('passgen.html', errors = errors, password = password)
+
+    else :
+
+        return render_template('passgen.html', errors = errors)
+
+
+@app.route('/link/shorten')
+def link_shortener():
+
+    errors = {}
+
+    if request.method == 'POST' :
+        url = request.form['fullurl']   #Get Url from form!
+        short_link = shorten_url(url)   # TODO:Add support for multiline links!
+
+        return render_template('shortener.html', errors=errors, short_link=short_link)
+    else :
+
+        return render_template('shortener.html', errors=errors)
+
---
+++ helper/helper.py
@@ -0,0 +1,9 @@
+
+
+def passgen(length,symbol=True,number=True) :
+
+    return 'abcd'
+
+def shorten_url(url):
+
+    return 's'+str(url)
